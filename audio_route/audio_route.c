5520f264d9b0e290dbbecf2e6035163b21a1b165
Baruch Eruchimovitch <baruche@codeaurora.org>
2013-11-02 21:36:11 +0200

    Nullify non-initialized entries in the mixer control structure
    While initialization process of some internal control structure,
    some entries can remain  non-initialized.
    To avoid possible crash while the structure release,
    the non-initialized entries should be zeroed.
    
    Change-Id: I2450c9214eeb478cc57feef706d00b10c6897b60

--- audio_route/audio_route.c
+++ audio_route/audio_route.c
@@ -550,8 +550,12 @@ static int alloc_mixer_state(struct audio_route *ar)
         /* Skip unsupported types that are not supported yet in XML */
         type = mixer_ctl_get_type(ctl);
 
-        if (!is_supported_ctl_type(type))
+        if (!is_supported_ctl_type(type)) {
+            ar->mixer_state[i].old_value = NULL;
+            ar->mixer_state[i].new_value = NULL;
+            ar->mixer_state[i].reset_value = NULL;
             continue;
+        }
 
         if (type == MIXER_CTL_TYPE_BYTE)
             value_sz = sizeof(unsigned char);
